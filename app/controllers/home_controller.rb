80ed2fd010dd2cd61f34a270b2096adab75e3439
Park Kyeul <gyul611@gmail.com>
2018-05-12 10:25:11 +0000

    no_name_project

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,11 +3,22 @@ class HomeController < ApplicationController
   end
 
   def new
-    # @posts = Post.order("created_at DESC").page params[:page]
-    # end
+  # 페이지 네이트
+     @posts = Post.order("created_at DESC").page params[:page]
   
   end
 
   def create
+    @post = Post.new
+    @post.title = params[:post_title]
+    @post.content = params[:post_content]
+    @post.save
+    
+    redirect_to '/home/new'
   end
+  
+  def notice
+     @posts = Post.all
+  end
+
 end
---
+++ app/models/post.rb
@@ -0,0 +1,4 @@
+class Post < ApplicationRecord
+    #  페이지네이트
+     paginates_per 10 
+end
