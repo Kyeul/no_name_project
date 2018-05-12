9b96bc6e1ef8c940a4338d2649d883e69293db72
Park Kyeul <gyul611@gmail.com>
2018-05-12 06:54:36 +0000

    no_name_project

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,13 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def new
+    # @posts = Post.order("created_at DESC").page params[:page]
+    # end
+  
+  end
+
+  def create
+  end
+end
